dc69bf28c65d331dedb812bb61716c11c97fc4b3
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2023-01-29 13:03:03 +0600

    layout

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom"
 import Main from "../../Layout/Main"
+import Bill from "../../Pages/Bill/Bill";
 import Home from "../../Pages/Home/Home"
+import Login from "../../Pages/Login/Login";
 
 const router = createBrowserRouter([
     {
@@ -10,7 +12,15 @@ const router = createBrowserRouter([
             {
                 path:"/",
                 element:<Home></Home>
-            }
+            },
+        {
+            path:'/login',
+            element:<Login></Login>
+        },
+        {
+            path:'/bill',
+            element:<Bill></Bill>
+        },
         ]
     }
 ])
